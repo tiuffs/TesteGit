86609b8a210eac98628fe1b311decd0dbab4d16c
Daniel Di Domenico <daniel.domenico@uffs.com.br>
2012-02-13 15:54:20 -0200

    Alteração efetuada através do notebook.
    Signed-off-by: Daniel Di Domenico <daniel.domenico@uffs.com.br>

--- src/Hello.java
+++ src/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world! Congratulations.");
+		System.out.println("Hello world! Alterado no notebook.");
 	}
 }
